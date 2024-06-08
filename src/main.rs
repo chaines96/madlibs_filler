2a81252976a7723ea4893da07b6821cbd1d8846f
chaines96 <haineschris777@gmail.com>
2024-06-08 17:16:23 -0600

    Functionality for replacing the text in the file was placed in its own function outside of main.

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,6 @@ fn main() -> std::io::Result<()> {
         wordlist_path_ptr = &args[1];
         madlib_path_ptr = &args[2];
     }
-    println!("In query {}", wordlist_path);
-    println!("In file {}", madlib_path);
     
     //Reading the file.
     let madlib_unsolved = fs::read_to_string(madlib_path_ptr)
@@ -28,29 +26,40 @@ fn main() -> std::io::Result<()> {
                 //Reads the list of words
     let wordlist = fs::read_to_string(wordlist_path_ptr)
             .expect("Error: Wordlist file missing!");
-    let mut verbs: Vec<_> = wordlist.lines().collect();
-    //Randomizes order of words
-    let mut rng = thread_rng();
-    verbs.shuffle(&mut rng);
-    //
+
+
     // Creates a mutable madlib string
     let mut madlib_solved = String::new();
-    // This 
-    let verb_splits = madlib_unsolved.split("<v>");
-    let mut i: usize = 0;
-    let mut j: usize = 0;
-    let verblist_length: usize = verbs.len().try_into().unwrap();
-    for verb_split in verb_splits {
-        //This conditional prevents an extra line being placed first
-        if j > 0 {
-            madlib_solved.push_str(verbs[i]);
-            }
-        madlib_solved.push_str(verb_split); //Previously used .replacen(<v>,"fart",1);
-        j = j + 1;
-        i = (j - 1) % verblist_length;
-    }
+    madlib_solved.push_str(&transform(wordlist, madlib_unsolved, "<v>"));
     let mut f = fs::File::create("output.txt")?;
     f.write_all(madlib_solved.as_bytes())?;
+    
     //writeln!(&mut f, madlib_unsolved);
     Ok(())
+}
+
+fn transform(wordlist: String, madlib_unsolved: String, delimiter: &str) -> String {
+        let mut yvqf:  Vec<&str> = wordlist.split(delimiter).collect();
+        let mut verbs: Vec<_> = yvqf[1].lines().collect();
+            //Randomizes order of words
+        let mut rng = thread_rng();
+        verbs.shuffle(&mut rng);
+    
+        let mut newthing = String::new();
+        // This 
+        let verb_splits = madlib_unsolved.split(delimiter);
+        let mut i: usize = 0;
+        let mut j: usize = 0;
+        let verblist_length: usize = verbs.len().try_into().unwrap();
+        for verb_split in verb_splits {
+            //This conditional prevents an extra line being placed first
+            if j > 0 {
+                newthing.push_str(verbs[i]);
+                }
+            newthing.push_str(verb_split); //Previously used .replacen(<v>,"fart",1);
+            j = j + 1;
+            i = (j - 1) % verblist_length;
+        }
+        
+    return newthing.to_string();
 }
